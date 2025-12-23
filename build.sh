5855916e2db1edc50a220ab0a4fb096574c2c648
UltimateQuack <patoflamejantetv@gmail.com>
2025-12-23 15:32:53 -0300

    Update build.sh
    Signed-off-by: UltimateQuack <patoflamejantetv@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # small script to easuly build the app
 
+cd .. # dumb bugfix
 flatpak run org.flatpak.Builder --force-clean --install --repo=./repo ./build ./gnome-chess/org.gnome.Chess.json --user
 #                                                                             our path                           needed to make
 #                                                                                                           flatpak shut the fuck up
